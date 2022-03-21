c7a5de219f490768612346414b5e8a3fd2d57243
AchrafASSILA <achrafassila678@gmail.com>
2022-03-21 13:47:53 +0100

    upload

--- classes/db.class.php
+++ classes/db.class.php
@@ -7,6 +7,9 @@ class Db
     private $dsn = 'mysql:root=localhost;dbname=proshop';
     private $user = 'root';
     private $password = '';
+    // private $dsn = 'mysql:host=sql201.epizy.com;dbname=epiz_31340851_proshop_db';
+    // private $user = 'epiz_31340851';
+    // private $password = 'ZRm3OSmDJuNyXKp';
     public function connect()
     {
         try {
